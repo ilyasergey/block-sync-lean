4821d0b3b8a38953dbb8db7cf37d7c6d0b465d52
Ilya Sergey <ilya@nus.edu.sg>
2026-04-26 13:44:32 +0800

    cleanup

--- BlockSynchroniser/Mysticeti/Liveness.lean
+++ BlockSynchroniser/Mysticeti/Liveness.lean
@@ -358,10 +358,10 @@ lemma three_consecutive_honest_direct_commit
     (system : BlockSynchroniserSystem)
     (time : TimeMap) (h_time : time.WellFormed)
     (h_sync : PartiallySynchronous system (belugaTrace system) time)
-    (hN : system.n = 3 * system.f + 1)
-    (hHonest : (system.validators.filter (fun p => p.2 = true)).length
+    (_hN : system.n = 3 * system.f + 1)
+    (_hHonest : (system.validators.filter (fun p => p.2 = true)).length
                 = 2 * system.f + 1)
-    (h_ids : ∀ i < system.n, ∃ pair ∈ system.validators, pair.1 = i)
+    (_h_ids : ∀ i < system.n, ∃ pair ∈ system.validators, pair.1 = i)
     (startRound : Round) (k₀ : ℕ) (h_gst : time k₀ ≥ system.GST) :
     ∃ r₁ ≥ startRound, ∃ k' ≥ k₀,
       (∀ B_L ∈ (belugaTrace system k').blocks,
